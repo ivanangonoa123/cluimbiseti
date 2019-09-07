f0f8b1983851de4f1877338f32d7d89fd4732841
Ivan Angonoa <ivan.X.Angonoa.-ND@disney.com>
2019-09-07 18:03:35 -0300

    expo linear gradient/custom fonts

--- App.js
+++ App.js
@@ -1,19 +1,56 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import * as Font from 'expo-font';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  state = {
+    fontLoaded: false,
+  };
+  async componentDidMount() {
+    await Font.loadAsync({
+      'Press Start 2P': require('./assets/fonts/PressStart2P-Regular.ttf'),
+    });
+    
+    this.setState({ fontLoaded: true })
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <LinearGradient
+          colors={['#7ad4d6', '#388b8f']}
+          style={styles.gradient}>
+        {
+          this.state.fontLoaded ? (<Text style={styles.title}>CLUIMBISETI</Text>) : null
+        }
+        </LinearGradient>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  gradient: {
+    width: '100%',
+    height: '100%',
     alignItems: 'center',
     justifyContent: 'center',
+    flex: 1,
   },
+  container: {
+    flex: 1
+  },
+  title: {
+    fontFamily: 'Press Start 2P',
+    fontSize: 30,
+    textShadowColor: "#000",
+    textShadowOffset: {
+      width: 2,
+      height: 2,
+    },
+    textShadowRadius:10,
+    elevation: 5,
+    color: '#fff'
+  }
 });
