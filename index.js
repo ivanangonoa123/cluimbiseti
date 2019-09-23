9f6bae6d77fcf596d29cc360da44e59a96b3b8e9
Ivan Angonoa <ivan.X.Angonoa.-ND@disney.com>
2019-09-23 00:31:19 -0300

    hide yellowbox unneeded warnings

--- index.js
+++ index.js
@@ -2,8 +2,11 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import {AppRegistry, YellowBox} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+
+YellowBox.ignoreWarnings(['Remote debugger']);
+YellowBox.ignoreWarnings(['componentWillReceiveProps']);
 
 AppRegistry.registerComponent(appName, () => App);
